80993e2656febf893229e421b75ee55790908fe1
sivakrishna <sivakrishnam356@gmail.com>
2025-09-08 14:53:10 +0530

    jquery exercise1 second commit

--- jquery/jquery-script.js
+++ jquery/jquery-script.js
@@ -1,76 +1,30 @@
+/* **********************
+    css changes using jquery 
+***************************** */ 
+
 $(document).ready(function () {
-    // for changing the css for resume_container
-    resume_container();
 
-    // Add styling class to input boxes
-    input_box();
+    // resume container css changes 
+    $(".change-bg").addClass("highlight-bg");
+    $(".change-bg").addClass("border");
 
-    // Hide the footer
-    footer_content();
+    // header css changes
+    $("#mypage-header").addClass("border-bottom");
+    $("#mypage-header").addClass("highlight-bg");
 
-    // Add references section above footer
-    add_references();
+    // input box
+    $(".input-box").addClass("input-field");
+
+    // section title border color
+    $(".section-title").addClass("border-color");
+
+    // content text color 
+    $(".change-color").addClass("text-color");
+
+    // hiding footer
+    $("#mypage-footer").hide();
+
+    // showing refernce div
+    $("#references").show().css("display", "flex");
 });
 
-/* *******************************************
-    changing resume-container css using jquery 
-   ******************************************** */
-function resume_container() {
-    // Change ID for
-    let resume_container = $("#resume-container");
-    resume_container.addClass("body-content");
-}
-
-/* *******************************************
-    changing input-box css using jquery 
-   ******************************************** */
-function input_box() {
-    // Adding a new class to all input fields
-    let input_box = $(".input-box");
-    input_box.addClass("input-field");
-}
-
-
-/* *******************************************
-            Hiding the footer 
-   ******************************************** */
-function footer_content() {
-    // Hides footer
-    let footer_content = $("#mypage-footer");
-    footer_content.hide();
-}
-
-/* *******************************************
-    Adding new div to the container before footer
-   ******************************************** */
-function add_references() {
-    // Adding a new div with before the footer
-    let footer_content = $("#mypage-footer");
-    footer_content.before('<div id="references"></div>');
-
-    // Add class
-    $('#references').addClass("section-row");
-
-    // Adding text to the div
-    $('#references').html(`
-        <div class="section-title"><h2>References</h2></div>
-        <div class="section-content">
-            <p>
-                <strong>Ryan Ramirez</strong> – Product Lead<br>
-                Seashore Enterprises<br>
-                187 Clive Road, House Springs, Missouri 63940<br>
-                (636) 423-2759<br>
-                <em>Ryan was my direct manager from March 2018 until March 2020</em>
-            </p>
-            <p>
-                <strong>Michael Brown</strong> – Director of Operations<br>
-                FutureTech Enterprises<br>
-                9102 Oak Boulevard, Rivertown, TX 75001<br>
-                (555) 555-1212
-            </p>
-        </div>
-    `);
-
-    // For showing refernce section
-    $('#references').show();
-}
